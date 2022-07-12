c8079e0c6a5bbf2b8b29e2c67d864b83926c1291
ben waldee <b1.waldee@gmail.com>
2022-07-12 16:24:13 -0500

    phase 2

--- server/app.js
+++ server/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 require('express-async-errors');
+//phase 1
 
 app.use('/static', express.static('assets'))
+
+//going into the folder from the requrl
+//grabbing wahts in there
+//res.send(what it grabbed)
+//responding will stop the code below from running (without a next)
 
 app.use(express.json());
 
@@ -24,6 +30,38 @@ app.post('/test-json', (req, res, next) => {
 app.get('/test-error', async (req, res) => {
   throw new Error("Hello World!")
 });
+
+
+//phase 2
+app.use((req, res, next) => {
+  console.log(req.method)
+  console.log(req.url)
+
+  res.on('finish', () => {
+    console.log(res.statusCode)
+  });
+
+  next()
+})
+
+
+app.use((req, res, next) => {
+  const err = new Error("The requested resource couldn't be found.")
+  err.statusCode = 404
+  next(err)
+})
+
+app.use((err, req, res, next) => {
+
+  const errCode = err.statusCode || 500
+  console.log("message", err.message,
+    "statusCode", errCode)
+  res.json({
+    "message": err.message,
+    "statusCode": errCode
+  })
+
+})
 
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
